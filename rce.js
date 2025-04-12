e3cd2f504a4a89f6cb20d1268273519c33c6d768
Nikoloz tskitishvili <ramimltsk@gmail.com>
2025-04-12 14:56:23 +0400

    Add files via upload

---
+++ rce.js
@@ -0,0 +1,10 @@
+const btn1 = document.querySelector(".btn1")
+
+const btn2 = document.querySelector(".btn2")
+
+const mainimage = document.querySelector(".mainimage")
+
+const images = document.querySelectorAll(".imagemain")
+
+
+
